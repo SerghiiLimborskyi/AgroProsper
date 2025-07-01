6af59eb8e64eac636996d7244b972448ef57a07b
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-07-01 06:41:37 +0200

    Update index.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,2 +1,8 @@
 const leaderboard = require('./api/referral-leaderboard');
 app.use('/api', leaderboard);
+const express = require('express');
+const app = express();
+const registerSeller = require('./api/register-seller');
+
+app.use(express.json());
+app.use('/api', registerSeller);
