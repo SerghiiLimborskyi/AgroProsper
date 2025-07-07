7b72c58bf29eba37ae2dbeb103b6469a08ea5b8d
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-07-08 01:46:21 +0200

    Delete docs/bot.py
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- docs/bot.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привіт! Я живий 🟢")
-
-app = ApplicationBuilder().token("7171552317:AAEQa_tW8Ae-81ccqbZ6AqspWZGLmJvLzs0").build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
