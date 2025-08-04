be67b742b6e88299cc48e3f8e5eea8579f04d341
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-04 18:08:02 +0200

    Update mint-api.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- docs/mint-api.js
+++ docs/mint-api.js
@@ -1,5 +1,27 @@
 // mint-api.js
 import { connectWallet, mintNFT } from './web3-utils.js';
+import React, { useEffect } from "react";
+import { logAppEvent } from "../utils/analytics";
+
+const Docs = () => {
+  useEffect(() => {
+    logAppEvent("screen_view", { screen_name: "Docs" });
+  }, []);
+
+  const handleDownload = (docName) => {
+    logAppEvent("document_downloaded", { name: docName });
+  };
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>📄 Документи</h1>
+      <button onClick={() => handleDownload("DAO_Whitepaper")}>Завантажити Whitepaper</button>
+    </div>
+  );
+};
+
+export default Docs;
+
 
 export async function handleMint(role, nodeId) {
   try {
