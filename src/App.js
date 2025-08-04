d1a20beb4965b911898fb580cdafbe707fb1145d
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-04 12:37:17 +0200

    Update App.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 // src/App.js
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
