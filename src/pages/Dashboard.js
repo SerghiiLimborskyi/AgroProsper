c0d620a73008c67003b9c37f1c70c754d46601ee
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-04 10:38:59 +0200

    Update and rename Dashboard.jsу to Dashboard.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+// src/pages/Dashboard.js
+
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>📈 Панель користувача</h1>
+      <p>Ласкаво просимо! Тут ви можете переглядати свої дані, аналітику та доступні функції.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
