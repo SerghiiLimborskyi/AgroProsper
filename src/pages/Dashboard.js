2b036981f4a7ff64b2d18bbe7df617fe6a164aef
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-04 16:54:15 +0200

    Update Dashboard.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,22 @@
 // src/components/Dashboard.js
 import React from "react";
+import { useEffect } from "react";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCky1vtPkr0p_Mzs6bCrlLqWAT1jK6fFTg",
+  authDomain: "agroprosper-1749411381988.firebaseapp.com",
+  projectId: "agroprosper-1749411381988",
+  storageBucket: "agroprosper-1749411381988.firebasestorage.app",
+  messagingSenderId: "398072332519",
+  appId: "1:398072332519:web:6a99edec387576e5e96242",
+  measurementId: "G-H36REQ6J6Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const Dashboard = ({ user, onSignOut }) => {
   return (
