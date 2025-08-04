e7f795a0c849c3bb459dd170098a03c86448d3cd
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-04 18:04:35 +0200

    Update Login.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,29 @@
 import React, { useEffect } from "react";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
+import React, { useEffect } from "react";
+import { logAppEvent } from "../utils/analytics";
+
+const Login = ({ onLogin }) => {
+  useEffect(() => {
+    logAppEvent("screen_view", { screen_name: "Login" });
+  }, []);
+
+  const handleLogin = () => {
+    logAppEvent("login_attempt", { method: "Google" });
+    onLogin();
+  };
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>🔐 Увійти до AgroProsper</h1>
+      <button onClick={handleLogin}>Увійти через Google</button>
+    </div>
+  );
+};
+
+export default Login;
+
 
 const firebaseConfig = {
   // ... той самий конфіг, що й у Dashboard.js
