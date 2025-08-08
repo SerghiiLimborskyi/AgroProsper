7dae912c7c8bc72a04cfd2ecfe19607a1b97ebe0
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-08 19:27:16 +0200

    Update hardhat.config.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,14 @@
-require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
-  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 31337
-    },
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
     }
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || ""
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
