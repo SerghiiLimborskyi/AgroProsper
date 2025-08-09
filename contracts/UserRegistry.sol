003c8f88e1fde8a7a553e6d7d38290a82df4106f
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-09 21:50:46 +0200

    Update UserRegistry.sol
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- contracts/UserRegistry.sol
+++ contracts/UserRegistry.sol
@@ -2,25 +2,21 @@
 pragma solidity ^0.8.0;
 
 contract UserRegistry {
-    enum Role { Farmer, Advertiser, Analyst, Donor }
-
     struct User {
-        address wallet;
-        Role role;
-        bool registered;
+        string name;
+        string email;
     }
 
-    mapping(address => User) public users;
+    mapping(string => User) private users;
+    mapping(string => bool) private registeredEmails;
 
-    event UserRegistered(address indexed wallet, Role role);
-
-    function registerUser(Role _role) external {
-        require(!users[msg.sender].registered, "Already registered");
-        users[msg.sender] = User(msg.sender, _role, true);
-        emit UserRegistered(msg.sender, _role);
+    function registerUser(string memory name, string memory email) public {
+        require(!registeredEmails[email], "User already registered");
+        users[email] = User(name, email);
+        registeredEmails[email] = true;
     }
 
-    function getUser(address _wallet) external view returns (User memory) {
-        return users[_wallet];
+    function isUserRegistered(string memory email) public view returns (bool) {
+        return registeredEmails[email];
     }
 }
