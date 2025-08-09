4692c208bbc66a1ce2418b9f377b21f2defefa62
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-09 23:31:46 +0200

    Update UserBadgeNFT.sol
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- contracts/UserBadgeNFT.sol
+++ contracts/UserBadgeNFT.sol
@@ -1,11 +1,34 @@
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-contract UserRegistry is ERC721URIStorage {
-    uint256 public badgeCounter;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-    function mintBadge(address user, string memory tokenURI) public onlyAdmin {
-        require(isUser(user), "Not registered");
-        _mint(user, badgeCounter);
-        _setTokenURI(badgeCounter, tokenURI);
-        badgeCounter++;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract UserBadgeNFT is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
+
+    mapping(string => string) public badgeURIs;
+
+    constructor() ERC721("UserBadge", "UBADGE") {
+        tokenCounter = 0;
+
+        // 🔖 URI для типів бейджів
+        badgeURIs["Starter"] = "ipfs://Qm...starter";
+        badgeURIs["Farmer"] = "ipfs://Qm...farmer";
+        badgeURIs["Trader"] = "ipfs://Qm...trader";
+    }
+
+    function mintBadge(address to, string memory badgeType) external onlyOwner {
+        require(bytes(badgeURIs[badgeType]).length > 0, "Invalid badge type");
+
+        uint256 newTokenId = tokenCounter;
+        _safeMint(to, newTokenId);
+        _setTokenURI(newTokenId, badgeURIs[badgeType]);
+
+        tokenCounter++;
+    }
+
+    function setBadgeURI(string memory badgeType, string memory uri) external onlyOwner {
+        badgeURIs[badgeType] = uri;
     }
 }
