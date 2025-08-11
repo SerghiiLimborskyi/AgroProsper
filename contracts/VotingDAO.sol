94e4bd27e28bc8b41cb631c748d34204b9824068
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-11 09:13:15 +0200

    Update VotingDAO.sol
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- contracts/VotingDAO.sol
+++ contracts/VotingDAO.sol
@@ -1,33 +1,79 @@
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract VotingDAO {
+    address public admin;
+    uint public proposalCount;
+
     struct Proposal {
         string description;
-        uint256 voteCount;
+        uint voteCount;
         bool active;
     }
 
-    mapping(uint256 => Proposal) public proposals;
-    mapping(uint256 => mapping(address => bool)) public hasVoted;
-    uint256 public proposalCount;
+    mapping(uint => Proposal) public proposals;
+    mapping(address => bool) public users;
+    mapping(uint => mapping(address => bool)) public votes;
 
-    event ProposalCreated(uint256 indexed id, string description);
-    event Voted(address indexed voter, uint256 indexed proposalId);
-
-    function createProposal(string memory description) public {
-        Proposal storage p = proposals[proposalCount];
-        p.description = description;
-        p.active = true;
-        proposalCount++;
-        emit ProposalCreated(proposalCount - 1, description);
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not admin");
+        _;
     }
 
-    function vote(uint256 proposalId) public {
-        Proposal storage p = proposals[proposalId];
-        require(p.active, "Proposal inactive");
-        require(!hasVoted[proposalId][msg.sender], "Already voted");
-        p.voteCount++;
-        hasVoted[proposalId][msg.sender] = true;
-        emit Voted(msg.sender, proposalId);
+    modifier onlyUser() {
+        require(users[msg.sender], "Not a DAO member");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+        users[msg.sender] = true;
+    }
+
+    function addUser(address user) public onlyAdmin {
+        users[user] = true;
+    }
+
+    function removeUser(address user) public onlyAdmin {
+        require(users[user], "User not found");
+        users[user] = false;
+    }
+
+    function changeAdmin(address newAdmin) public onlyAdmin {
+        require(newAdmin != address(0), "Invalid address");
+        admin = newAdmin;
+    }
+
+    function createProposal(string memory description) public onlyUser {
+        proposals[proposalCount] = Proposal(description, 0, true);
+        proposalCount++;
+    }
+
+    function vote(uint proposalId) public onlyUser {
+        require(proposals[proposalId].active, "Voting closed");
+        require(!votes[proposalId][msg.sender], "Already voted");
+
+        votes[proposalId][msg.sender] = true;
+        proposals[proposalId].voteCount++;
+    }
+
+    function closeProposal(uint proposalId) public onlyAdmin {
+        proposals[proposalId].active = false;
+    }
+
+    function getAllProposals() public view returns (Proposal[] memory) {
+        Proposal[] memory result = new Proposal[](proposalCount);
+        for (uint i = 0; i < proposalCount; i++) {
+            result[i] = proposals[i];
+        }
+        return result;
+    }
+
+    function isVotingOpen(uint proposalId) public view returns (bool) {
+        return proposals[proposalId].active;
+    }
+
+    function getVoteCount(uint proposalId) public view returns (uint) {
+        return proposals[proposalId].voteCount;
     }
 }
