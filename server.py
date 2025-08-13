cc134f8abb21b59b45d26b64c0a96ba050fece44
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-13 16:21:39 +0200

    Create server.py
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

---
+++ server.py
@@ -0,0 +1,10 @@
+POST /api/referral-transition
+Content-Type: application/json
+
+{
+  "timestamp": "2025-08-13T16:00:00",
+  "referral_tag": "AGT-XYZ123",
+  "user_data": {
+    "browser_fingerprint": "abc123xyz"
+  }
+}
