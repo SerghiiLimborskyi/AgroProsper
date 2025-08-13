308b3086107722da0b3d78c968a87a1ff0f83af8
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-13 16:22:29 +0200

    Create python server.py
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

---
+++ python server.py
@@ -0,0 +1,10 @@
+{
+  "status": "success",
+  "data": {
+    "timestamp": "2025-08-13T16:00:00",
+    "referral_tag": "AGT-XYZ123",
+    "user_data": {
+      "browser_fingerprint": "abc123xyz"
+    }
+  }
+}
