843bcef0e251cd449ea8d50b15f2faa25504604d
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-31 11:48:42 +0200

    Create tailwind.config.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

---
+++ docs/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dao: '#00FF66',
+        bg: '#1A1A1A'
+      }
+    }
+  },
+  plugins: []
+}
