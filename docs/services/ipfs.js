c0cd46b17ccb7cd4bc231d9b817d4c88ef9b2a13
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-31 12:27:57 +0200

    Update ipfs.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- docs/services/ipfs.js
+++ docs/services/ipfs.js
@@ -1,2 +1,13 @@
 import { create } from 'ipfs-http-client';
 export const ipfs = create({ url: 'https://ipfs.io' });
+import { Web3Storage, File } from 'web3.storage';
+
+const token = 'YOUR_WEB3STORAGE_API_TOKEN';
+const client = new Web3Storage({ token });
+
+export async function uploadMetadata(metadata) {
+  const blob = new Blob([JSON.stringify(metadata)], { type: 'application/json' });
+  const file = new File([blob], 'metadata.json');
+  const cid = await client.put([file]);
+  return `https://ipfs.io/ipfs/${cid}/metadata.json`;
+}
