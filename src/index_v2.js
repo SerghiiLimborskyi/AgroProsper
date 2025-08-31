1dbc214e27cc13b0a25a4e68cea71621491787a3
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-31 11:52:13 +0200

    Update index_v2.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- src/index_v2.js
+++ src/index_v2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RegisterForm from "./RegisterForm"; // або VotingPanel, якщо хочеш DAO
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
