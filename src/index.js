ece63a8eff01838959669272d328b0360d9e6e02
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-08-31 11:51:05 +0200

    Update index.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RegisterForm from './RegisterForm';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RegisterForm />);
