2837308665c82d27d071e80cad81f1553dd3fef5
AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>
2025-09-01 09:23:25 +0200

    Delete games/badgeVerifier.js
    Signed-off-by: AgroProsper <84835838+SerghiiLimborskyi@users.noreply.github.com>

--- games/badgeVerifier.js
+++
@@ -1,5 +0,0 @@
-export async function hasBadge(address) {
-  const provider = new ethers.BrowserProvider(window.ethereum);
-  const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
-  return await contract.hasBadge(address);
-}
